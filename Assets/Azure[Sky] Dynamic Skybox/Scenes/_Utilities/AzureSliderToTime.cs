b2f145e4813133b41b3aeb50149112332e36d36d
daniliammo <67186086+daniliammo@users.noreply.github.com>
2024-08-14 23:53:54 +0500

    0

---
+++ Assets/Azure[Sky] Dynamic Skybox/Scenes/_Utilities/AzureSliderToTime.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.UI;
+
+namespace UnityEngine.AzureSky
+{
+    public class AzureSliderToTime : MonoBehaviour
+    {
+        public AzureTimeController azureTimeController;
+
+        public void SetAzureTimeline(Slider timeSlider)
+        {
+            azureTimeController.SetTimeline(timeSlider.value);
+        }
+    }
+}
