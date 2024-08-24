2f00ba784bcc6a300f16e3776c5c73e838b2ac2a
daniliammo <67186086+daniliammo@users.noreply.github.com>
2024-08-24 23:58:47 +0500

    FisherYatesListShuffle.cs перенесен в папку Utils

---
+++ Assets/Scripts/Utils/FisherYatesListShuffle.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace Utils
+{
+    public static class FisherYatesListShuffle
+    {
+        
+        private static readonly Random Random = new();  
+
+        public static void Shuffle<T>(this IList<T> list)  
+        {  
+            var n = list.Count;  
+            while (n > 1) 
+            {  
+                n--;  
+                var k = Random.Next(n + 1);  
+                (list[k], list[n]) = (list[n], list[k]);
+            }  
+        }
+        
+    }
+}
