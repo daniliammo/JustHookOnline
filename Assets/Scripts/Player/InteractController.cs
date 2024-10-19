5b794d524d43f965317d779f4d3d9f005a672f42
DANILIAMMO <67186086+daniliammo@users.noreply.github.com>
2024-10-19 23:09:31 +0500

    remove unused usings

--- Assets/Scripts/Player/InteractController.cs
+++ Assets/Scripts/Player/InteractController.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Text;
 using Interactable;
 using JetBrains.Annotations;
 using Mirror;
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using Cars;
 using GameSettings;
 using Mirror;
